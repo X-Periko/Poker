11096096c6798aff105935bb715bb45c80294fbd
Pablo Martínez Prado <138979989+X-Periko@users.noreply.github.com>
2024-09-02 12:52:41 +0000

    aguacatr

--- Poker.py
+++ Poker.py
@@ -7,9 +7,11 @@ free_cards = ['p1', 'p2', 'p3', 'p4', 'p5', 'p6', 'p7', 'p10', 'p11', 'p12',
               't1', 't2', 't3', 't4', 't5', 't6', 't7', 't10', 't11', 't12',
               'd1', 'd2', 'd3', 'd4', 'd5', 'd6', 'd7', 'd10', 'd11', 'd12',
               'c1', 'c2', 'c3', 'c4', 'c5', 'c6', 'c7', 'c10', 'c11', 'c12']
-AI_cards = []
-Player_cards = []
-
+AI_cards = [0,0,0,0,0]
+Player_cards = [0,0,0,0,0]
+pairs_count = None
+HasFull = None
+HasPoker = None
 
 
 #Comprobar parejas
@@ -79,77 +81,78 @@ def CheckPairs():
 #Comprobar tríos
 
 trios = []
-for x in range (0,5):
-    for y in range(0,5):
-        for z in range(0,5):
-            if AI_cards[x] == AI_cards[y]:
-                continue
-            if AI_cards[x] == AI_cards[z]:
-                continue
-            if AI_cards[y] == AI_cards[z]:
-                continue
-            if '1' in AI_cards[x] and '1' in AI_cards[y] and '1' in AI_cards[z]:
-                if not len(AI_cards[x]) == 3 and not len(AI_cards[y]) == 3 and not len(AI_cards[z] == 3):
+def CheckTrios ():
+    for x in range (0,5):
+        for y in range(0,5):
+            for z in range(0,5):
+                if AI_cards[x] == AI_cards[y]:
+                    continue
+                if AI_cards[x] == AI_cards[z]:
+                    continue
+                if AI_cards[y] == AI_cards[z]:
+                    continue
+                if '1' in AI_cards[x] and '1' in AI_cards[y] and '1' in AI_cards[z]:
+                    if not len(AI_cards[x]) == 3 and not len(AI_cards[y]) == 3 and not len(AI_cards[z] == 3):
+                        if AI_cards[x] not in trios and AI_cards[y] not in trios and AI_cards[z] not in trios:    
+                            trios.append(AI_cards[x])
+                            trios.append(AI_cards[y])
+                            trios.append(AI_cards[z])
+                        print(AI_cards[x], ' and ', AI_cards[y], ' and ', AI_cards[z])
+                if '2' in AI_cards[x] and '2' in AI_cards[y] and '2' in AI_cards[z]:
+                    if not len(AI_cards[x]) == 3 and not len(AI_cards[y]) == 3 and not len(AI_cards[z] == 3):
+                        if AI_cards[x] not in trios and AI_cards[y] not in trios and AI_cards[z] not in trios:    
+                            trios.append(AI_cards[x])
+                            trios.append(AI_cards[y])
+                            trios.append(AI_cards[z])
+                        print(AI_cards[x], ' and ', AI_cards[y], ' and ', AI_cards[z])
+                if '3' in AI_cards[x] and '3' in AI_cards[y] and '3' in AI_cards[z]:
                     if AI_cards[x] not in trios and AI_cards[y] not in trios and AI_cards[z] not in trios:    
                         trios.append(AI_cards[x])
                         trios.append(AI_cards[y])
                         trios.append(AI_cards[z])
                     print(AI_cards[x], ' and ', AI_cards[y], ' and ', AI_cards[z])
-            if '2' in AI_cards[x] and '2' in AI_cards[y] and '2' in AI_cards[z]:
-                if not len(AI_cards[x]) == 3 and not len(AI_cards[y]) == 3 and not len(AI_cards[z] == 3):
+                if '4' in AI_cards[x] and '4' in AI_cards[y] and '4' in AI_cards[z]:
                     if AI_cards[x] not in trios and AI_cards[y] not in trios and AI_cards[z] not in trios:    
                         trios.append(AI_cards[x])
                         trios.append(AI_cards[y])
                         trios.append(AI_cards[z])
                     print(AI_cards[x], ' and ', AI_cards[y], ' and ', AI_cards[z])
-            if '3' in AI_cards[x] and '3' in AI_cards[y] and '3' in AI_cards[z]:
-                if AI_cards[x] not in trios and AI_cards[y] not in trios and AI_cards[z] not in trios:    
-                    trios.append(AI_cards[x])
-                    trios.append(AI_cards[y])
-                    trios.append(AI_cards[z])
-                print(AI_cards[x], ' and ', AI_cards[y], ' and ', AI_cards[z])
-            if '4' in AI_cards[x] and '4' in AI_cards[y] and '4' in AI_cards[z]:
-                if AI_cards[x] not in trios and AI_cards[y] not in trios and AI_cards[z] not in trios:    
-                    trios.append(AI_cards[x])
-                    trios.append(AI_cards[y])
-                    trios.append(AI_cards[z])
-                print(AI_cards[x], ' and ', AI_cards[y], ' and ', AI_cards[z])
-            if '5' in AI_cards[x] and '5' in AI_cards[y] and '5' in AI_cards[z]:
-                if AI_cards[x] not in trios and AI_cards[y] not in trios and AI_cards[z] not in trios:    
-                    trios.append(AI_cards[x])
-                    trios.append(AI_cards[y])
-                    trios.append(AI_cards[z])
-                print(AI_cards[x], ' and ', AI_cards[y], ' and ', AI_cards[z])
-            if '6' in AI_cards[x] and '6' in AI_cards[y] and '6' in AI_cards[z]:
-                if AI_cards[x] not in trios and AI_cards[y] not in trios and AI_cards[z] not in trios:    
-                    trios.append(AI_cards[x])
-                    trios.append(AI_cards[y])
-                    trios.append(AI_cards[z])
-                print(AI_cards[x], ' and ', AI_cards[y], ' and ', AI_cards[z])
-            if '7' in AI_cards[x] and '7' in AI_cards[y] and '7' in AI_cards[z]:
-                if AI_cards[x] not in trios and AI_cards[y] not in trios and AI_cards[z] not in trios:    
-                    trios.append(AI_cards[x])
-                    trios.append(AI_cards[y])
-                    trios.append(AI_cards[z])
-                print(AI_cards[x], ' and ', AI_cards[y], ' and ', AI_cards[z])
-            if '10' in AI_cards[x] and '10' in AI_cards[y] and '10' in AI_cards[z]:
-                if AI_cards[x] not in trios and AI_cards[y] not in trios and AI_cards[z] not in trios:    
-                    trios.append(AI_cards[x])
-                    trios.append(AI_cards[y])
-                    trios.append(AI_cards[z])
-                print(AI_cards[x], ' and ', AI_cards[y], ' and ', AI_cards[z])
-            if '11' in AI_cards[x] and '11' in AI_cards[y] and '11' in AI_cards[z]:
-                if AI_cards[x] not in trios and AI_cards[y] not in trios and AI_cards[z] not in trios:    
-                    trios.append(AI_cards[x])
-                    trios.append(AI_cards[y])
-                    trios.append(AI_cards[z])
-                print(AI_cards[x], ' and ', AI_cards[y], ' and ', AI_cards[z])
-            if '12' in AI_cards[x] and '12' in AI_cards[y] and '12' in AI_cards[z]:
-                if AI_cards[x] not in trios and AI_cards[y] not in trios and AI_cards[z] not in trios:    
-                    trios.append(AI_cards[x])
-                    trios.append(AI_cards[y])
-                    trios.append(AI_cards[z])
-                print(AI_cards[x], ' and ', AI_cards[y], ' and ', AI_cards[z])
+                if '5' in AI_cards[x] and '5' in AI_cards[y] and '5' in AI_cards[z]:
+                    if AI_cards[x] not in trios and AI_cards[y] not in trios and AI_cards[z] not in trios:    
+                        trios.append(AI_cards[x])
+                        trios.append(AI_cards[y])
+                        trios.append(AI_cards[z])
+                    print(AI_cards[x], ' and ', AI_cards[y], ' and ', AI_cards[z])
+                if '6' in AI_cards[x] and '6' in AI_cards[y] and '6' in AI_cards[z]:
+                    if AI_cards[x] not in trios and AI_cards[y] not in trios and AI_cards[z] not in trios:    
+                        trios.append(AI_cards[x])
+                        trios.append(AI_cards[y])
+                        trios.append(AI_cards[z])
+                    print(AI_cards[x], ' and ', AI_cards[y], ' and ', AI_cards[z])
+                if '7' in AI_cards[x] and '7' in AI_cards[y] and '7' in AI_cards[z]:
+                    if AI_cards[x] not in trios and AI_cards[y] not in trios and AI_cards[z] not in trios:    
+                        trios.append(AI_cards[x])
+                        trios.append(AI_cards[y])
+                        trios.append(AI_cards[z])
+                    print(AI_cards[x], ' and ', AI_cards[y], ' and ', AI_cards[z])
+                if '10' in AI_cards[x] and '10' in AI_cards[y] and '10' in AI_cards[z]:
+                    if AI_cards[x] not in trios and AI_cards[y] not in trios and AI_cards[z] not in trios:    
+                        trios.append(AI_cards[x])
+                        trios.append(AI_cards[y])
+                        trios.append(AI_cards[z])
+                    print(AI_cards[x], ' and ', AI_cards[y], ' and ', AI_cards[z])
+                if '11' in AI_cards[x] and '11' in AI_cards[y] and '11' in AI_cards[z]:
+                    if AI_cards[x] not in trios and AI_cards[y] not in trios and AI_cards[z] not in trios:    
+                        trios.append(AI_cards[x])
+                        trios.append(AI_cards[y])
+                        trios.append(AI_cards[z])
+                    print(AI_cards[x], ' and ', AI_cards[y], ' and ', AI_cards[z])
+                if '12' in AI_cards[x] and '12' in AI_cards[y] and '12' in AI_cards[z]:
+                    if AI_cards[x] not in trios and AI_cards[y] not in trios and AI_cards[z] not in trios:    
+                        trios.append(AI_cards[x])
+                        trios.append(AI_cards[y])
+                        trios.append(AI_cards[z])
+                    print(AI_cards[x], ' and ', AI_cards[y], ' and ', AI_cards[z])
 
 trioscount = int(len(trios) / 3)
 print('I have ', trioscount, ' trios')
@@ -158,22 +161,24 @@ if len(trios) > 0:
 
 #Identificar full
 
-if len(pairs) >= 2 and len(trios) >= 3:
-    HasFull = True
-else:
-    HasFull = False
+def CheckFull():
+    if len(pairs) >= 2 and len(trios) >= 3:
+        HasFull = True
+    else:
+        HasFull = False
 
 #Identificar póker
 
-for x in range(0,5):
-    for y in range(0,5):
-        for z in range(0,5):
-            for n in range(0,5):
-                if 'p' in AI_cards[x] and 'p' in AI_cards[y] and 'p' in AI_cards[z] and 'p' in AI_cards[n]:
-                    HasPoker = True
-                    break
-                else: 
-                    HasPoker = False
+def ChackPoker():
+    for x in range(0,5):
+        for y in range(0,5):
+            for z in range(0,5):
+                for n in range(0,5):
+                    if 'p' in AI_cards[x] and 'p' in AI_cards[y] and 'p' in AI_cards[z] and 'p' in AI_cards[n]:
+                        HasPoker = True
+                        break
+                    else: 
+                        HasPoker = False
 
 #Apuesta inicial y reparto de dinero
 
@@ -211,6 +216,7 @@ root.mainloop()
 #Juego
 while Player_money > 0 and AI_money > 0:
     def shuffleAI():
+        AI_cards = []
         selected_card =  r.choice(free_cards)
         selected_index = free_cards.index(selected_card)
         del free_cards[selected_index]
@@ -221,6 +227,7 @@ while Player_money > 0 and AI_money > 0:
     print(AI_cards)
 
     def shuffleP():
+        Player_cards = []
         selected_card =  r.choice(free_cards)
         selected_index = free_cards.index(selected_card)
         del free_cards[selected_index]
@@ -229,6 +236,9 @@ while Player_money > 0 and AI_money > 0:
     for x in range(0,5):
         shuffleP()
     print(Player_cards)
+
+    CheckTrios()
+    CheckPairs()
 
     Desea_subir = 'Desea subir la apuesta o retirarse (s/r)? Tiene ', Player_money, ' dólares'
     print(Desea_subir)
